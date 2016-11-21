eaeeb2b4497cc63924e8b351df3dbf176ade3a72
Joshua Mirth <joshua@Luminuma.local>
2016-11-20 18:51:46 -0700

    Improved file selection process.

--- ProcessExcelMWE.m
+++ ProcessExcelMWE.m
@@ -5,23 +5,31 @@ function ProcessExcelMWE()
 % creation of the file containing correction data and file names. A future
 % TODO is to create a script which can generate these (semi)-automatically.
 
-% Change to the folder containing the data files.
-working_dir = pwd;
-data_dir = uigetdir;
-cd(data_dir);
 
-% Load the data file in this folder.
-% This should be a .mat file in struct format. The first line should give
-% the title of each compatible Excel file in the folder. The second line
-% should have the corresponding correction factors.
-load file_listing.mat;
+% Skip this automation step for now. TODO: maybe come back and write this
+% in a more automated way eventually.
+% % Change to the folder containing the data files.
+% working_dir = pwd;
+% data_dir = uigetdir;
+% cd(data_dir);
+% 
+% % Load the data file in this folder.
+% % This should be a .mat file in struct format. The first line should give
+% % the title of each compatible Excel file in the folder. The second line
+% % should consist of 1s and 0s, for whether the files has already been 
+% % processed or not.
+% data = load(file_listing.mat);
+% number_files = length(data);
+
+data = uigetfile('*.xlsx','Select all files to process','MultiSelect','on');
+
 number_files = length(data);
 
 % Process each Excel file.
 for i = 1:number_files
-    fileName = data(i).fileName;
+    fileName = data{i};
     % Determine how many sheets are in the file
-    [~,sheets] = xlsinfo(fileName);
+    [~,sheets] = xlsfinfo(fileName);
     number_sheets = length(sheets);
     % Store each of the components of the data.
     % NOTE: These data files are huge. XLSRead is very slow.This will take 
@@ -48,13 +56,25 @@ for i = 1:number_files
     acc_s = downsample(acc_s_vec,100);
     
     % Compute the associated posture
-    theta = AccelToPosture(acc_v,acc_l,acc_s);
+    theta = AccelToPosture(acc_l,acc_s,acc_v);
+    
+    % Apply the correction factor
+    if length(theta) > 45
+        correction_factor = mean(theta(15:45));
+        theta = theta + correction_factor;
+    else
+        disp('Cannot apply correction to',filename);
+    end
     
     % Save the downsampled data to a .mat file.
     downsampled_data = [times,acc_v,acc_l,acc_s,theta];
     save_name = strcat(name,'_downsampled','.mat');
     save(save_name,downsampled_data);
     
+    % Do the CEVA
+    bins = zeros(1,9);
+    bins = AccelerationCEVA(theta);
+    disp(bins);
 end
 
 end
